9707964b764f2195ae190c539ca65bc1c70a0d88
rwithik <rwithik@gmail.com>
2020-01-13 07:16:55 +0530

    nav, mobile view

---
+++ scripts/main.js
@@ -0,0 +1,16 @@
+const navButton = document.querySelector(".navbar-toggle");
+const navList = document.querySelector(".navbar__list");
+
+navButton.addEventListener("click", () => {
+  navList.classList.toggle("navbar__list--open");
+  document.querySelector(".hamburger").classList.toggle("hamburger--close");
+});
+
+const navItems = document.querySelectorAll(".navbar__item");
+
+navItems.forEach(item => {
+  item.addEventListener("click", () => {
+    navList.classList.remove("navbar__list--open");
+    console.log("fire");
+  });
+});
