3c3a9d1a1857dca433769754a1b35cd3cc340680
rwithik <rwithik@gmail.com>
2020-01-13 07:45:04 +0530

    bugfix for background-clip

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,10 @@
 const navButton = document.querySelector(".navbar-toggle");
 const navList = document.querySelector(".navbar__list");
+const hamburger = document.querySelector(".hamburger");
 
 navButton.addEventListener("click", () => {
   navList.classList.toggle("navbar__list--open");
-  document.querySelector(".hamburger").classList.toggle("hamburger--close");
+  hamburger.classList.toggle("hamburger--close");
 });
 
 const navItems = document.querySelectorAll(".navbar__item");
@@ -11,6 +12,6 @@ const navItems = document.querySelectorAll(".navbar__item");
 navItems.forEach(item => {
   item.addEventListener("click", () => {
     navList.classList.remove("navbar__list--open");
-    console.log("fire");
+    hamburger.classList.toggle("hamburger--close");
   });
 });
